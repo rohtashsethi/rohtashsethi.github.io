31768f76be0be885194b2d22b79e4499898598b7
Rohtash Sethi <rohtash.sethi@gmail.com>
2025-04-20 22:58:52 +0530

    customixed checkbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProjectCardComponent } from './shared/components/project-card/project-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProjectCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
